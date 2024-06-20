45da8b7cb5fe640e999da01682150ec2243dd3d3
akshay2s <akshay.aadrika@gmail.com>
2024-06-20 17:27:48 +0530

    changes

---
+++ citizen_mb_build/assets/WaterApplicationPayment-6f4eb8be.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-92e4a43a.js";const e=()=>t("div",{children:"WaterApplicationPayment"});export{e as default};
---
+++ citizen_mb_build/assets/useSetTitle-1f0277af.js
@@ -0,0 +1,1 @@
+import{R as s,f as o,r as a}from"./index-92e4a43a.js";function c(t){const{settitleText:e}=s.useContext(o);a.useEffect(()=>{e(t)},[])}export{c as u};
